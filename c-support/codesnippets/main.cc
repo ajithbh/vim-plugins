0959540dc502600987277def3b9f0a6e25a842ce
Ajith <ajith@ajith-desktop.(none)>
2014-09-16 15:35:56 +0800

    Modified to personal preferences.

--- c-support/codesnippets/main.cc
+++ c-support/codesnippets/main.cc
@@ -1,19 +1,17 @@
 #include <cstdlib>
 #include <fstream>
-#include <iomanip>   
-#include <iostream>  
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
-// ===  FUNCTION  ======================================================================
+// =====================================================================================
 //         Name:  main
 //  Description:  main function
 // =====================================================================================
-	int
-main ( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-	cout	<< "\nProgram " << argv[0] << endl << endl;
+    cout << "\nProgram " << argv[0] << endl << endl;
 
-	return EXIT_SUCCESS;
-}		// ----------  end of function main  ---------- 
-
+    return EXIT_SUCCESS;
+}
